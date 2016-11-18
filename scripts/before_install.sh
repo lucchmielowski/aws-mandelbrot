6b1b95ea9270392c15115a7f7297cdd6c44da462
Luc Chmielowski <luc.chmielowski@gmail.com>
2016-11-18 02:04:26 +0100

    modified scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,10 @@ yum update -y
 yum upgrade -y
 
 # install dependencies
-yum install mercurial git gcc libc6-dev nginx go -y
+yum install mercurial git gcc libc6-dev go -y
+
+service nginx stop
+service nginx start
 
 # install golang
 cd /home/ec2-user
